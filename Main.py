d22914b5493f972ca257afcea836da3fb82cf86f
michael <michaelrantohi@gmail.com>
2025-03-16 23:26:06 -0400

    Split code more logically, created separate class PingTool for pinging addresses.

--- Main.py
+++ Main.py
@@ -1,46 +1,40 @@
 from NetworkInfo import NetworkInfo
+from PingTool import PingTool
+import logging
+from datetime import datetime
+
+logging.basicConfig(filename="net_log.txt", level=logging.INFO)
 
 ni = NetworkInfo()
-ips_to_ping = []
+pt = PingTool()
 inp = None
 
 print("Welcome to NetworkChecker!")
+print("\nWould you like to log your Network Info? (Y/N)")
+inp = input().lower().strip()
+if inp.lower() in ["y", "yes"]:
+    net_info = f"{datetime.now()}:  Local IP: {ni.get_socket()}, Gateway: {ni.get_default_gateway()}, DNS: {ni.get_dns_nameservers()}"
+    print(net_info)
+    logging.info(net_info)
+
+elif inp.lower() in ["n", "no"]:
+    print("Skipping logging of Network Info...")
+else:
+    print("Invalid input. Please enter 'Y' or 'N'.")
 print("Input IPs to ping, if done ENTER")
 while (inp != ""):
     inp = input()
     if inp == "":
         break
-    ips_to_ping.append(inp)
-if not ips_to_ping:
+    pt.add_ip(inp)
+if not pt.get_addresses():
     print("No IP addresses were entered to ping.")
 else:
-    ni.ping_global(ips_to_ping)
+    print(pt.ping_addresses())
 
-print("\n\nWould you like to get the Local IP Address? (Y/N)")
-inp = input().lower().strip()
-if inp.lower() in ["y", "yes"]:
-    print(ni.get_socket())
-elif inp.lower() in ["n", "no"]:
-    print("Skipping Local IP Address request.")
-else:
-    print("Invalid input. Please enter 'Y' or 'N'.")
 
-print("\n\nWould you like to get the Default Gateway IP? (Y/N)")
-if inp.lower() in ["y", "yes"]:
-    print(ni.get_default_gateway())
-elif inp.lower() in ["n", "no"]:
-    print("Skipping Default Gateway IP Address request.")
-else:
-    print("Invalid input. Please enter 'Y' or 'N'.")
 
-print("\n\nWould you like to get the DNS Nameservers? (Y/N)")
-inp = input()
-if inp.lower() in ["y", "yes"]:
-    print(ni.get_dns_nameservers())
-elif inp.lower() in ["n", "no"]:
-    print("Skipping DNS Nameserver request.")
-else:
-    print("Invalid input. Please enter 'Y' or 'N'.")
+
 
 
 
--- NetworkInfo.py
+++ NetworkInfo.py
@@ -1,31 +1,9 @@
-import logging
-import subprocess
 import socket
 import ifaddr
-import platform
-from datetime import datetime
-
 
 class NetworkInfo:
-    logging.basicConfig(filename="net_log.txt", level = logging.INFO)
-
-    # Detects OS user is using and then pings requested addresses.
-    # Tells success of the ping based on the returncode.
-    def ping_global(self, addresses):
-        result = "ERROR!"
-        for ip in addresses:
-            ping_cmd = ['ping', '-c', '1', ip] if platform.system() == 'Windows' else ['ping', '-c', '1', ip]
-            p = subprocess.run(ping_cmd)
-            if p.returncode == 0:
-                result = (f"{ip} is up")
-            else:
-                result = (f"{ip} is down")
-        logging.info(f"\n{datetime.now()}: Ping result: {result}")
-
-
     def get_socket(self):
         socket_ip = (socket.gethostbyname(socket.gethostname())) # Gets local IP address
-        logging.info(f"\n{datetime.now()}: Socket IP: {socket_ip}")
         return socket_ip
 
 
@@ -34,7 +12,6 @@ class NetworkInfo:
         for adapter in adapters:
             if adapter.nice_name == 'en0':
                 dg = adapter.ips[1].ip # Returns default gateway IP address
-                logging.info(f"\n{datetime.now()}: Default Gateway: {dg}")
                 return dg
 
     def get_dns_nameservers(self):
@@ -43,7 +20,6 @@ class NetworkInfo:
                 if line.startswith("nameserver"):
                     line = line.split(" ")
                     dns = line[1]
-                    logging.info(f"\n{datetime.now()}: DNS: {dns}")
                     return dns
 
 
