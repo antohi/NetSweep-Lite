cb37e49fab499cc43b84b2a6d102e49bccf11dff
michael <michaelrantohi@gmail.com>
2025-04-07 14:14:16 -0400

    Added PingTool module for multiIP pinging w/ logging and concurrency

---
+++ PingTool.py
@@ -0,0 +1,67 @@
+import subprocess
+import platform
+import logging
+import concurrent.futures
+import re
+
+
+class PingTool:
+    def __init__(self):
+        self.addresses = []
+
+    # Add IPs to the list
+    def add_ip(self, ip):
+        self.addresses.append(ip)
+
+    #Get addys
+    def get_addresses(self):
+        return self.addresses
+
+    # Pings a single IP
+    def ping_ip(self, ip):
+        try:
+            ping_cmd = ['ping', '-n', '4', ip] if platform.system() == 'Windows' else ['ping', '-c', '4', ip]
+            p = subprocess.run(ping_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+            if p.returncode == 0:
+                self.get_detailed_info(str(p), ip)
+                logging.info(f"[SUCCESS]: Ping to {ip} was successful")
+                return f"{ip} is UP"
+            else:
+                logging.warning(f"[FAIL]: Ping to {ip} was unsuccessful. Return code: {p.returncode}")
+                return f"{ip} is DOWN"
+        except Exception as e:
+            logging.error(f"[EXCEPTION]: {e}" )
+            return f"[ERROR] {ip} is DOWN"
+    #concurrently pings all IP addresses
+    def ping_addresses(self):
+        statuses = []  # List to accumulate results
+
+        # using ThreadPoolExecutor for parallel pinging
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            results = executor.map(self.ping_ip, self.addresses)  # Executes ping_ip for each address in parallel
+
+        # printing results to console
+        for result in results:
+            print(result)
+
+        return "\n".join(statuses)
+
+    def get_detailed_info(self, p, ip):
+        # Latency info
+        latency_info = re.search(r"(\d+\.\d+)/(\d+\.\d+)/(\d+\.\d+)", p)
+        min, avg, max = latency_info.groups()
+        logging.info(f"{ip} [LATENCY] Minimum: {min}, Average: {avg}, Maximum: {max}")
+
+        # Packet info
+        packets_transmitted = re.search(r"(\d) packets transmitted", p)
+        packets_received = re.search(r"(\d) packets received", p)
+        packets_lost = re.search(r"(\d+\.\d+)% packet loss", p)
+
+        transmitted = packets_transmitted.group(1) if packets_transmitted else "N/A"
+        received = packets_received.group(1) if packets_received else "N/A"
+        lost = packets_lost.group(1) if packets_lost else "N/A"
+        logging.info(f"{ip}: [PACKETS] Transmitted: {transmitted}, Received: {received}, Lost: {lost}%")
+
+
+
+
