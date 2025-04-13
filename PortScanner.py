3b1cc7fe6364024448cf9d7e75d3efad2bea5db6
michael <michaelrantohi@gmail.com>
2025-04-13 11:30:25 -0400

    Added screenshots of functionality to project

--- PortScanner.py
+++ PortScanner.py
@@ -1,11 +1,13 @@
 import re
 import subprocess
-class PortScanner:
+import logging
 
+class PortScanner:
     # Scan a single port based on an IP address
     def scan_single_port(self, port, host):
         scan_cmd = ["nmap", "-p", port, host]
         s = subprocess.run(scan_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        logging.info(s.stdout)
         return s.stdout
 """
     def get_port_info(self, p):
