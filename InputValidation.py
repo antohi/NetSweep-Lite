e7d78bb6166d0420b08ff928044364bcbf73afa8
michael <michaelrantohi@gmail.com>
2025-04-17 11:50:09 -0400

    Created static method validate_ip to check if user-entered IP is valid

---
+++ InputValidation.py
@@ -0,0 +1,12 @@
+import ipaddress
+
+
+class InputValidation:
+
+    @staticmethod
+    def validate_ip(ip):
+        try:
+            ipaddress.ip_address(ip)
+            return True
+        except ValueError:
+            return False
