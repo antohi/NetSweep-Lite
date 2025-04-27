802b1fab8639a8e5ea96b7784bf9041692e1901d
michael <michaelrantohi@gmail.com>
2025-04-27 11:42:11 -0400

    Working on regex scan, polish up UI a little. Still need to make format for regex scan better

--- PortScanner.py
+++ PortScanner.py
@@ -12,7 +12,7 @@ class PortScanner:
         return self.format_scan(s.stdout)
 
     # Formats output of scan to make more readable when printing/logging
-    def format_scan(self, s):
+    def format_port_scan(self, s):
         ports = re.findall(r"(\d+)/(tcp|udp)\s+(open|closed|filtered)\s+(\S+)", s)
         formatted = []
         for port, protocol, state, service in ports:
@@ -21,13 +21,22 @@ class PortScanner:
             logging.info(port)
             print(port)
 
+    def format_banner_scan(self, s):
+        banners = re.findall(r"(\d+)/(tcp|udp)\s+(open|closed|filtered)\s+(\w+)(.+)", s)
+        formatted = []
+        for port, protocol, state, service, version in banners:
+            formatted.append(f"{port}/{protocol} - {state.upper()} ({service} | Version: {version})")
+        for banner in formatted:
+            logging.info(banner)
+            print(banner)
+
     # Scans top 10 ports using nmap's --top-ports functionality
     def quick_scan(self, host):
         scan_cmd = ["nmap", "--top-ports", "10", host]
         s = subprocess.run(scan_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
         logging.info("\n\n===QUICK SCAN RESULTS===")
         print("\n\n===QUICK SCAN RESULTS===")
-        return self.format_scan(s.stdout)
+        return self.format_port_scan(s.stdout)
 
     # Scans top 25 ports using nmap's --top-ports functionality
     def deep_scan(self, host):
@@ -35,10 +44,11 @@ class PortScanner:
         s = subprocess.run(scan_cmod, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
         logging.info("\n\n===DEEP SCAN RESULTS===")
         print("\n\n===DEEP SCAN RESULTS===")
-        return self.format_scan(s.stdout)
+        return self.format_port_scan(s.stdout)
 
     # Nmap banners detection
     def scan_banners(self, host):
         scan_cmd = ["nmap", "-sV", host]
         s = subprocess.run(scan_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-        return s.stdout
+        print("\n\n===BANNER SCAN RESULTS===")
+        return self.format_banner_scan(s.stdout)
