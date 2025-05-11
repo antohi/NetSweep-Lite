1cca9fec95f016450f36a7e4fa96428c54eb03ec
michael <michaelrantohi@gmail.com>
2025-05-11 11:54:13 -0400

    Added test_process_banners in test class to check if process_banners is logging correctly

--- tests/test_riskscanner.py
+++ tests/test_riskscanner.py
@@ -30,7 +30,7 @@ class DummyJSONResponse:
             ]
         }
 
-# Tests check_risk function with dummy JSON data
+# Tests check_risk func with dummy JSON data
 def test_check_risk(monkeypatch):
     monkeypatch.setattr(requests, "get", lambda *args, **kwargs: DummyJSONResponse())
 
@@ -40,8 +40,8 @@ def test_check_risk(monkeypatch):
     assert results[0]['cve_id'] == 'CVE-TEST'
     assert 'Test description' in results[0]['description']
 
+# Tests format_risks func to verify proper results are printed and filtered out
 def test_format_risks(capsys):
-
     scanner = RiskScanner()
     results = [
         {"cve_id": "CVE-LOW",   "description": "Low severity",       "severity": "LOW",      "score": 2.0},
@@ -61,6 +61,25 @@ def test_format_risks(capsys):
     assert "CVE-LOW" not in captured
     assert "Score:" in captured
 
+
+def test_process_banners(monkeypatch, capsys):
+    scanner = RiskScanner()
+    dummy_cves = [
+        {
+            "cve_id": "CVE-dumb-dumb",
+            "description": "dumb-dumb description",
+            "severity": "HIGH",
+            "score": 7.1
+        }
+    ]
+
+    monkeypatch.setattr(scanner, "check_risk", lambda product: dummy_cves)
+    banner_output = "22/tcp open ssh OpenSSH 7.9\n"
+
+    scanner.process_banners(banner_output)
+    captured = capsys.readouterr().out
+
+    assert "PORT: 22/tcp | STATE: OPEN | SERVICE: ssh | VERSION: OpenSSH 7.9" in captured
 
 
 
